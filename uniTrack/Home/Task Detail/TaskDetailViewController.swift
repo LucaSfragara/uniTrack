b759018928bd279919a3421f29837b8d5b7ebfcd
Luca Sfragara <lucasfragara@gmail.com>
2020-12-18 23:34:43 +0100

    add task detail and deadline detail

---
+++ uniTrack/Home/Task Detail/TaskDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TaskDetailViewController.swift
+//  uniTrack
+//
+//  Created by Luca Sfragara on 18/12/20.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
