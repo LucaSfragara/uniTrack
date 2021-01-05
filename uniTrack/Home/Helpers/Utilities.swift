e67a3d73df5d21899557edcf333a5983321ea796
Luca Sfragara <lucasfragara@gmail.com>
2021-01-05 18:09:21 +0100

    add getCountry with name and  flags

--- uniTrack/Home/Helpers/Utilities.swift
+++ uniTrack/Home/Helpers/Utilities.swift
@@ -23,14 +23,43 @@ class Utilities{
         return alert
     }
     
-    static func getCountryList() -> [String]{
+    static func getCountryList() -> [Country]{
         
-        var countries: [String] = []
+        var countries: [Country] = []
         for code in NSLocale.isoCountryCodes  {
             let id = NSLocale.localeIdentifier(fromComponents: [NSLocale.Key.countryCode.rawValue: code])
             let name = NSLocale(localeIdentifier: "en_UK").displayName(forKey: NSLocale.Key.identifier, value: id) ?? "Country not found for code: \(code)"
-            countries.append(name)
+            countries.append(Country(name: name, isoCountryCode: code))
         }
         return countries
     }
+}
+
+struct Country{
+    
+    var name: String
+    var isoCountryCode: String
+    
+    var flag: String?{
+        func isLowercaseASCIIScalar(_ scalar: Unicode.Scalar) -> Bool {
+                    return scalar.value >= 0x61 && scalar.value <= 0x7A
+                }
+
+                func regionalIndicatorSymbol(for scalar: Unicode.Scalar) -> Unicode.Scalar {
+                    precondition(isLowercaseASCIIScalar(scalar))
+
+                    // 0x1F1E6 marks the start of the Regional Indicator Symbol range and corresponds to 'A'
+                    // 0x61 marks the start of the lowercase ASCII alphabet: 'a'
+                    return Unicode.Scalar(scalar.value + (0x1F1E6 - 0x61))!
+                }
+
+                let lowercasedCode = isoCountryCode.lowercased()
+                guard lowercasedCode.count == 2 else { return nil }
+                guard lowercasedCode.unicodeScalars.reduce(true, { accum, scalar in accum && isLowercaseASCIIScalar(scalar) }) else { return nil }
+
+                let indicatorSymbols = lowercasedCode.unicodeScalars.map({ regionalIndicatorSymbol(for: $0) })
+                return String(indicatorSymbols.map({ Character($0) }))
+        
+    }
+    
 }
