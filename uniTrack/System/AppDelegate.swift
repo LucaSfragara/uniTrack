b43d091894ce3c15608fc45ace150614caf8c4d8
Luca Sfragara <lucasfragara@gmail.com>
2021-02-10 22:28:42 +0100

    Added launch screen

--- uniTrack/System/AppDelegate.swift
+++ uniTrack/System/AppDelegate.swift
@@ -24,7 +24,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
         if hasLaunchedBefore != true{
             //load universities json into coredata
             preloadUniversityData()
